c16fb7c3edeb2d010b9f3b0b3897744926b8f433
Anh Le Hoang <lehoanganh25991@gmail.com>
2019-03-22 10:49:28 +0700

    Update conf

--- swarmpit/run-swarmpit.sh
+++ swarmpit/run-swarmpit.sh
@@ -1,20 +1,17 @@
 # !/bin/bash
-SWARMPIT_COMPOSE_DIR=/opt/dokcer-compose/swarmpit
-SWARMPIT_COMPOSE_FILE=${SWARMPIT_COMPOSE_DIR}/docker-compose.yml
-
-DOMAIN=swarmpit.traefik.tinypos.org
 STACK_NAME=swarmpit
+DOMAIN=swarmpit.traefik.tinypos.org
+SWARMPIT_COMPOSE_DIR=/opt/docker-compose/swarmpit
+
+### Create directory if not exist
+docker-machine ssh manager "mkdir -p ${SWARMPIT_COMPOSE_DIR}"
 
 ### Upload compose file to MANAGER node
-docker-machine ssh manager "
-  mkdir -p ${SWARMPIT_COMPOSE_DIR}
-  touch ${SWARMPIT_COMPOSE_FILE}
-"
-docker-machine scp ./swarmpit/docker-compose.yml manager:${SWARMPIT_COMPOSE_FILE}
+docker-machine scp ./swarmpit/docker-compose.yml manager:${SWARMPIT_COMPOSE_DIR}/
 
 ### Run stack
 docker-machine ssh manager "docker stack rm ${STACK_NAME}"
-docker-machine ssh manager "DOMAIN=${DOMAIN} docker stack deploy --compose-file ${SWARMPIT_COMPOSE_FILE} ${STACK_NAME}"
+docker-machine ssh manager "DOMAIN=${DOMAIN} docker stack deploy --compose-file ${SWARMPIT_COMPOSE_DIR}/docker-compose.yml ${STACK_NAME}"
 
 ### Log info
 docker-machine ssh manager "docker stack ps ${STACK_NAME}"
